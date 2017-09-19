019c0b8039e44194a94d3dc341a8a5a1811efc49
Derek Fei <DerekFei@users.noreply.github.com>
2017-09-19 09:10:09 -0400

    struct

---
+++ structs/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "asdf@asdf.com",
+			zipCode: 343,
+		},
+	}
+	jim.updateName("Jimmy")
+	jim.print()
+
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
